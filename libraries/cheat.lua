c20e4997e032a32998d728984344ebbc54c15521
Toree <46419590+toreeeee@users.noreply.github.com>
2023-04-26 00:05:31 +0200

    feat: add missing members to cheat table

--- libraries/cheat.lua
+++ libraries/cheat.lua
@@ -14,5 +14,14 @@ function cheat:register_module(module) end
 ---@return c_module
 function cheat:get_module_by_champion(champion_name) end
 
+---@return string
+function cheat:get_resource_path() end
+
+---@return number
+function cheat:get_local_attack_cast_delay() end
+
+---@return number
+function cheat:get_local_attack_cast_delay() end
+
 ---@type cheat
 _G.cheat = {}
