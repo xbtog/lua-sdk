5abcdf9428e4d91a6b0301eddd1e7d9ddfd703ce
Toree <46419590+toreeeee@users.noreply.github.com>
2023-09-09 15:53:50 +0200

    feat: add missing member functions to `c_target_selector`

--- classes/c_target_selector.lua
+++ classes/c_target_selector.lua
@@ -11,4 +11,23 @@ function c_target_selector:get_secondary_target() end
 function c_target_selector:get_orbwalker_default_target() end
 
 ---@return c_object Returns current orbwalker target only when in AA range.
-function c_target_selector:get_orbwalker_default_target() end
+function c_target_selector:get_orbwalker_secondary_target() end
+
+---@return c_object | nil
+function c_target_selector:get_forced_target() end
+
+---@return boolean
+function c_target_selector:is_forced() end
+
+---@param index number
+---@param ignore_dead boolean | nil
+---@param ignore_invisible boolean | nil
+---@return boolean
+function c_target_selector:is_bad_target(index, ignore_dead, ignore_invisible) end
+
+---@param target number | c_object | nil
+---@return boolean
+function c_target_selector:force_target(target) end
+
+---@return c_object | nil
+function c_target_selector:get_antigapclose_target() end
