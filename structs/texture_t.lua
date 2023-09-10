d3d4736c671a21dff9f5744ad4224ccffbf6d10f
toreeeee <maybeguy1998@gmail.com>
2023-09-10 17:11:35 +0200

    feat: add missing members to `texture_t`

--- structs/texture_t.lua
+++ structs/texture_t.lua
@@ -2,6 +2,9 @@
 [View Documentation](https://slotted.cc/docs/structs/texture_t)
 ]]
 ---@class texture_t
+---@field public name string
+---@field public width number
+---@field public height number
 local texture_t = {}
 
 ---@type texture_t
