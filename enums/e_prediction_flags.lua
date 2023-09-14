ead2e13ac5d19ed19275fa1b0eac1e9e763244f9
toreeeee <maybeguy1998@gmail.com>
2023-09-14 14:40:54 +0200

    feat: add docs link to `e_prediction_flags`

--- enums/e_prediction_flags.lua
+++ enums/e_prediction_flags.lua
@@ -1,4 +1,7 @@
-﻿---@class e_prediction_flags
+﻿--[[
+[View Documentation](https://slotted.cc/docs/enums/e_prediction_flags)
+]]
+---@class e_prediction_flags
 ---@field public include_ping number @ = 1
 ---@field public render_thread number @ = 2
 ---@field public extend_range_with_hitbox number @ = 4
