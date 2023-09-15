3157e769fd65827a0f6c7d0b05dc1866d5d5c852
toreeeee <maybeguy1998@gmail.com>
2023-09-15 22:01:44 +0200

    fix: `c_evade:is_position_safe` arguments

--- classes/features/c_evade.lua
+++ classes/features/c_evade.lua
@@ -4,10 +4,11 @@
 local c_evade = {}
 
 ---@param position vec3
----@param safe_distance boolean
+---@param lowest_danger_level number | nil
 ---@param radius_modifier number | nil
+---@param is_blink boolean | nil
 ---@return boolean Returns true if the position is safe.
-function c_evade:is_position_safe(position, safe_distance, radius_modifier) end
+function c_evade:is_position_safe(position, lowest_danger_level, radius_modifier, is_blink) end
 
 ---@return boolean Returns true if evade is active.
 function c_evade:is_active() end
